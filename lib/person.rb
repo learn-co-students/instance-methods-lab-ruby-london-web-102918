2c61e88e310686344266083528b1b37d382805bc
itzcookiie <mayowaadeniyi@hotmail.com>
2018-10-30 23:59:44 +0000

    Done

---
+++ lib/person.rb
@@ -0,0 +1,12 @@
+class Person
+    def talk
+        puts "Hello World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+end
+    maya = Person.new
+    maya.talk
+    maya.walk
