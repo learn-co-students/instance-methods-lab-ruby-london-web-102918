7f4b211b98d21a4b6f7c5da925c1936437b75758
rosen777 <rosen.toshev@flatironschool.com>
2018-10-31 00:49:17 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+
+  # Instance method #bark
+  def bark
+    puts "Woof!"
+  end
+
+  # Instance method #sit
+  def sit
+    puts "The Dog is sitting"
+  end
+
+  def walk
+    puts "The Person is walking"
+  end
+
+end
